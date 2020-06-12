0a8d4101b9162da160d7fc705cf39fcbd268a130
Deep <pateldeep5724@gmail.com>
2020-06-12 21:32:11 +0530

    feat : ready to publish mean app generation
    use "npm i appstack-generator" to install and "generatemean" to generate mean stack app

--- index.js
+++ index.js
@@ -1,70 +1,48 @@
-'use strict';
-
+#!/usr/bin/env node
 const inquirer = require('./lib/utils/inquirer');
 const { exec } = require('child_process');
 const { stderr, stdout } = require('process');
 const { async } = require('rxjs/internal/scheduler/async');
 const directory = require('./lib/utils/files/directory');
+const { spawn } = require( 'child_process' );
+const chalk = require('chalk');
+
+
+var projectname;
 var expressname;
 
-
-const { spawn } = require( 'child_process' );
-// const ls = spawn( 'ng new hello' ,[]);
-// const ls= spawn('ng', ['new', 'service']);
-const ls= spawn('ng', ['new', 'service' ]);
-
-
-
-
 const name = async () => {
-  const res= await inquirer.askAppName();
-  expressname = res.appName;
-  return expressname;
+  const res= await inquirer.askProjectName();
+  projectname = res.appName;
+  return projectname;
 }
 
 
 const run = async () => {
+
+  
+  console.log(chalk.yellow("Generating Express Backend : "));
+
   await name(); 
-  const dir =await directory.createDir(expressname);
+  const dir =await directory.createDir(projectname);
   // console.log(dir);
 
-  const changesDir =await directory.changeWorkingDir(expressname);
-  console.log(changesDir);
+  const changesDir =await directory.changeWorkingDir(projectname);
+  // console.log(changesDir);
+  // console.log(`Generating Express Backend: ${expressname}`);
 
 
-  console.log(`Generating : ${expressname}`);
 
-  // exec("express expressBackend",  (error, stdout, stderr) => {
-  //   if (error) {
-  //     console.log('Error : ' + error);
-  //     return;
-  //   }
-  //   if (stderr) {
-  //     console.log('Error : ' + stderr);
-  //     return;
-  //   }
-  //   console.log('stdout : ' + stdout);
-  // })
+  const res= await inquirer.askExpressAppName();
+  expressname = res.appName;
 
-  require( "child_process" ).spawnSync( "sh", [ "-c", "ng new" ], { stdio: "inherit", stdin: "inherit" } );
+
+
   require( "child_process" ).spawnSync( "sh", [ "-c", `express ${expressname}` ], { stdio: "inherit", stdin: "inherit" } );
 
+  console.log(chalk.yellow(`Generating Angular Frontend : `));
 
-
-  // exec("ng new angularFrontend" , (error,stdout ,stderr)=>{
-  //   if (error) {
-  //         console.log('Error : ' + error);
-  //         return;
-  //       }
-  //       if (stderr) {
-  //         console.log('Error : ' + stderr);
-  //         return;
-  //       }
-  //       console.log('stdout : ' + stdout);
-  // });
-
-
-
+  require( "child_process" ).spawnSync( "sh", [ "-c", "ng new" ], { stdio: "inherit", stdin: "inherit" } );
 
 }
 
