2b4608968b87c9e47de34bda0051e7472f266f70
Ravi Agheda <42093885+raviagheda@users.noreply.github.com>
2020-06-12 07:57:38 +0530

    mean app

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-const inquirer = require('inquirer')
+// const inquirer = require('inquirer')
 const { exec } = require('child_process');
 const { stderr } = require('process');
 const { async } = require('rxjs/internal/scheduler/async');
 
-var expressname;
+// var expressname;
 
-var questions = [{
-  type: 'input',
-  name: 'name',
-  message: "Please enter express app name",
-}]
+// var questions = [{
+//   type: 'input',
+//   name: 'name',
+//   message: "Please enter express app name",
+// }]
 
 
-const name = async () => {
-  await inquirer.prompt(questions).then(answers => {
-    // console.log(`Hi ${answers['name']}!`);
-    expressname = answers['name'];
-    // return expressname;
-  })
-  return expressname;
-}
+// const name = async () => {
+//   await inquirer.prompt(questions).then(answers => {
+//     // console.log(`Hi ${answers['name']}!`);
+//     expressname = answers['name'];
+//     // return expressname;
+//   })
+//   return expressname;
+// }
 
 
 const run = async () => {
 
-  await name();
+  // await name();
   
-  console.log(`Generating : ${expressname}`);
+  console.log('Generating node app via express');
 
-  exec("express "+expressname,  (error, stdout, stderr) => {
+  // express app
+  exec("express node",  (error, stdout, stderr) => {
     if (error) {
       console.log('Error : ' + error);
       return;
@@ -39,6 +40,22 @@ const run = async () => {
     }
     console.log('stdout : ' + stdout);
   })
+
+  // angular app
+  console.log('Generating angular app');
+  exec("ng new angular",  (error, stdout, stderr) => {
+    if (error) {
+      console.log('Error : ' + error);
+      return;
+    }
+    if (stderr) {
+      console.log('Error : ' + stderr);
+      return;
+    }
+    console.log('stdout : ' + stdout);
+  })
+
+  console.log('Do not forget to call `npm install` in both app before you use!');
 
 }
 
